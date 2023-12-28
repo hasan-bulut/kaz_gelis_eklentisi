578c8a7ce75a7e1e7f59b938bb318246131294bf
Hasan Bulut <buluthasan853@gmail.com>
2023-12-28 22:06:51 +0300

    money commands added

--- BP/scripts/main.js
+++ BP/scripts/main.js
@@ -1,8 +1,50 @@
 import { world, system } from "@minecraft/server";
 import { JsonDatabase } from "./database/database";
+import { getMoney, setMoney, addMoney, removeMoney } from "./economy/money";
 
 const db = new JsonDatabase("database");
 db.load();
+
+const commandPrefix = "#";
+
+world.beforeEvents.chatSend.subscribe(event => {
+    var sender = event.sender;
+    var msg = event.message;
+
+    if (msg[0] == commandPrefix) {
+        msg = msg.substring(1);
+        var msgList = msg.split(" ");
+
+        switch (msgList[0]) {
+            case "param":
+                sender.sendMessage("Paran: " + getMoney(sender.name) + "TL");
+                break;
+            case "paraayarla":
+                sender.sendMessage(setMoney(sender.name, Number(msgList[1])));
+                break;
+            case "paraekle":
+                sender.sendMessage(addMoney(sender.name, Number(msgList[1])));
+                break;
+            case "parasil":
+                sender.sendMessage(removeMoney(sender.name, Number(msgList[1])));
+                break;
+        }
+    }
+
+    // var kgBlocks = db.get("kgBlocks") ?? [];
+
+    // if (msgList.length == 5 && msgList.includes("#kazgelis")) {
+    //     msgList.shift();
+    //     kgBlocks.push(msgList.join(","));
+    //     db.set("kgBlocks", kgBlocks);
+    // }
+
+    // if (msg == "clear") {
+    //     db.set("kgBlocks", []);
+    // }
+
+    // sender.sendMessage(kgBlocks.toString());
+});
 
 system.runInterval(() => {
     var kgBlocks = db.get("kgBlocks") ?? [];
@@ -11,49 +53,10 @@ system.runInterval(() => {
         var valueList = value.split(",");
         if (world.getDimension("overworld").getBlock({ x: parseInt(valueList[0], 10), y: parseInt(valueList[1], 10), z: parseInt(valueList[2], 10) }).isAir) {
             world.getDimension("overworld").fillBlocks({ x: parseInt(valueList[0], 10), y: parseInt(valueList[1], 10), z: parseInt(valueList[2], 10) }, { x: parseInt(valueList[0], 10), y: parseInt(valueList[1], 10), z: parseInt(valueList[2], 10) }, "bedrock");
+            system.runTimeout(() => {
+                world.sendMessage("bitti");
+                world.getDimension("overworld").fillBlocks({ x: parseInt(valueList[0], 10), y: parseInt(valueList[1], 10), z: parseInt(valueList[2], 10) }, { x: parseInt(valueList[0], 10), y: parseInt(valueList[1], 10), z: parseInt(valueList[2], 10) }, valueList[3]);
+            }, 60);
         }
     });
 })
-
-world.beforeEvents.playerBreakBlock.subscribe((event) => {
-    var player = event.player;
-    var block = event.block;
-    var kgBlocks = db.get("kgBlocks") ?? [];
-
-    kgBlocks.forEach(value => {
-        var valueList = value.split(",");
-        if (block.location.x == parseInt(valueList[0], 10) &&
-            block.location.y == parseInt(valueList[1], 10) &&
-            block.location.z == parseInt(valueList[2], 10)) {
-            player.sendMessage(event.block.typeId + " " + "minecraft:" + valueList[3] + " " + (event.block.typeId == "minecraft:" + valueList[3]).toString());
-            if (event.block.typeId == "minecraft:" + valueList[3]) {
-                system.runTimeout(() => {
-                    world.sendMessage("bitti");
-                    world.getDimension("overworld").fillBlocks({ x: parseInt(valueList[0], 10), y: parseInt(valueList[1], 10), z: parseInt(valueList[2], 10) }, { x: parseInt(valueList[0], 10), y: parseInt(valueList[1], 10), z: parseInt(valueList[2], 10) }, valueList[3]);
-                }, 60);
-            } else {
-                event.cancel = true;
-            }
-        }
-    });
-});
-
-world.beforeEvents.chatSend.subscribe(event => {
-    var sender = event.sender;
-    var msg = event.message;
-    var msgList = msg.split(" ");
-
-    var kgBlocks = db.get("kgBlocks") ?? [];
-
-    if (msgList.length == 5 && msgList.includes("#kazgelis")) {
-        msgList.shift();
-        kgBlocks.push(msgList.join(","));
-        db.set("kgBlocks", kgBlocks);
-    }
-
-    if (msg == "clear") {
-        db.set("kgBlocks", []);
-    }
-
-    sender.sendMessage(kgBlocks.toString());
-});
